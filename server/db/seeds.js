6172dc00685efff20a33841c22faec37765ea10b
Blair Scott <blairs4932@outlook.com>
2023-09-05 11:31:56 +0100

    added server, DB seeds, server routes, and link to DB

---
+++ server/db/seeds.js
@@ -0,0 +1,58 @@
+use GitBash;
+
+db.dropDatabase();
+
+db.characters.insertMany([
+  {
+    name: "Homer Simpson",
+    description: "Homer is the patriarch of the Simpson family. He works as a safety inspector at the Springfield Nuclear Power Plant, a position at odds with his careless, buffoonish personality. He is married to Marge Simpson, a stereotypical American housewife and mother. They have three children: Bart, a ten-year-old troublemaker; Lisa, a precocious eight-year-old activist; and Maggie, the baby of the family who rarely speaks, but communicates by sucking on a pacifier. Although Homer has many flaws, he has shown to have great caring, love, and even bravery to those he cares about and, sometimes, even others he doesn't. He served as the main protagonist of the 2007 film adaptation.",
+    moves: {
+      punch: {
+        damageMin: 10,
+        damageMax: 16,
+        defense: 10,
+      },
+      kick: {
+        damageMin: 15,
+        damageMax: 22,
+        defense: 5,
+      },
+      specialMove: {
+        name: "Donut Thunder",
+        damageMin: 30,
+        damageMax: 55,
+        defense: 3,
+      },
+    },
+    sprites: {
+      default: "./images/Homer/homer_default.png",
+      default_inverted: "./images/Homer/homer_default_inverted.png",
+    }
+  },
+  {
+    name: "Dug",
+    description: "Dug",
+    moves: {
+      punch: {
+        damageMin: 12,
+        damageMax: 18,
+        defense: 8,
+      },
+      kick: {
+        damageMin: 19,
+        damageMax: 26,
+        defense: 7,
+      },
+      specialMove: {
+        name: "Bulldug Slam",
+        damageMin: 25,
+        damageMax: 35,
+        defense: 0,
+      },
+    },
+    sprites: {
+      default: "./images/Dug/dug_default.png",
+      default_inverted: "./images/Dug/dug_default_inverted.png",
+    }
+  }
+]);
