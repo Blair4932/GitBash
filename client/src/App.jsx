17866c6de5d884da64f24bcef0ea2b298bf3cc1c
Ross Buchan <rossbuchan1@googlemail.com>
2023-09-05 15:41:33 +0100

    Create compareMoves function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
 import { useState, useEffect } from "react";
 
 function App() {
+
   const [characters, setCharacters] = useState([]);
+  const [playerMove, setPlayerMove] = useState({})
+  const [opponentMove, setOpponentMove] = useState({})
+  const [playerHealth, setPlayerHealth] = useState(100)
+  const [opponentHealth, setOpponentHealth] = useState(100)
+
   const fetchCharacters = () => {
     fetch("http://localhost:9000/api/characters/")
       .then((res) => res.json())
@@ -9,11 +15,47 @@ function App() {
         setCharacters(data);
       });
   };
+
   useEffect(() => {
     fetchCharacters();
+    setPlayerMove(
+      {
+        name: "punch",
+				damageMin: 10,
+				damageMax: 30,
+				defence: 10
+			}
+    )
+    setOpponentMove(
+      {
+        name: "kick",
+				damageMin: 15,
+				damageMax: 22,
+				defence: 5
+			}
+    )
   }, []);
-  console.log(characters);
-  return <></>;
+
+  console.log(characters)
+
+  const compareMoves = () => {
+    if (playerMove.name == "block" || opponentMove.name == "block") {
+      // probably want some kind of feedback here at some point but nothing functionally happens 
+    }
+    else {
+      const playerMoveDamage = Math.floor(Math.random() * (playerMove.damageMax - playerMove.damageMin + 1 )) + playerMove.damageMin
+      const opponentMoveDamage = Math.floor(Math.random() * (opponentMove.damageMax - opponentMove.damageMin + 1 )) + opponentMove.damageMin
+
+      playerMoveDamage > opponentMoveDamage 
+      ? setOpponentHealth(opponentHealth - (playerMoveDamage - opponentMove.defence)) 
+      : setPlayerHealth(playerHealth - (opponentMoveDamage - playerMove.defence))
+    }
+  };
+
+  return <>
+    <h1>{characters.length > 1 ? characters[0].name : "Loading"}</h1>
+    <button onClick={compareMoves}>FIGHT (test)</button>
+  </>;
 }
 
 export default App;
