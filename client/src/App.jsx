1c14f7531392deac0a58d79ddcfac8dce76dfe0e
Blair Scott <blairs4932@outlook.com>
2023-09-06 14:05:08 +0100

    Added Arena Select

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,139 +1,170 @@
-import { useState, useEffect } from 'react';
-import CharacterSelect from './components/CharacterSelect';
-import PlayerMovesModal from './components/PlayerMovesModal';
-import CharacterImage from './components/CharacterImage';
-import RoundCounterResultContainer from './containers/RoundCounterResultContainer';
+import { useState, useEffect } from "react";
+import "./App.css";
+import CharacterSelect from "./components/CharacterSelect";
+import PlayerMovesModal from "./components/PlayerMovesModal";
+import CharacterImage from "./components/CharacterImage";
+import RoundCounterResultContainer from "./containers/RoundCounterResultContainer";
+import ArenaSelect from "./components/ArenaSelect";
+import ArenaImage from "./components/ArenaImage";
+import styled from "styled-components";
 
 function App() {
-	const [characters, setCharacters] = useState([]);
-	const [selectedCharacter, setSelectedCharacter] = useState({});
-	const [opponentCharacter, setOpponentCharacter] = useState({});
-	const [playerMove, setPlayerMove] = useState({});
-	const [opponentMove, setOpponentMove] = useState({});
-	const [playerHealth, setPlayerHealth] = useState(100);
-	const [opponentHealth, setOpponentHealth] = useState(100);
-	const [result, setResult] = useState('');
-	const [roundTracker, setRoundTracker] = useState(0);
+  const [characters, setCharacters] = useState([]);
+  const [selectedCharacter, setSelectedCharacter] = useState({});
+  const [opponentCharacter, setOpponentCharacter] = useState({});
+  const [arenas, setArenas] = useState({});
+  const [selectedArena, setSelectedArena] = useState({});
+  const [playerMove, setPlayerMove] = useState({});
+  const [opponentMove, setOpponentMove] = useState({});
+  const [playerHealth, setPlayerHealth] = useState(100);
+  const [opponentHealth, setOpponentHealth] = useState(100);
+  const [result, setResult] = useState("");
+  const [roundTracker, setRoundTracker] = useState(0);
 
-	const fetchCharacters = () => {
-		fetch('http://localhost:9000/api/characters/')
-			.then((res) => res.json())
-			.then((data) => {
-				setCharacters(data);
-				setSelectedCharacter(data[0]);
-				setOpponentCharacter(data[0]);
-			});
-	};
+  const fetchCharacters = () => {
+    fetch("http://localhost:9000/api/characters/")
+      .then((res) => res.json())
+      .then((data) => {
+        setCharacters(data);
+        console.log(data);
+        console.log(characters);
+        setSelectedCharacter(data[0]);
+        setOpponentCharacter(data[0]);
+      });
+  };
 
-	useEffect(() => {
-		fetchCharacters();
-	}, []);
+  const fetchArenas = () => {
+    fetch("http://localhost:9000/api/arenas/")
+      .then((res) => res.json())
+      .then((data) => {
+        setArenas(data);
+        setSelectedArena(data[0]);
+      });
+  };
 
-	const compareMoves = () => {
-		setRoundTracker(roundTracker + 1);
-		if (playerMove.name == 'Block' || opponentMove.name == 'Block') {
-		} else {
-			const playerMoveDamage =
-				Math.floor(
-					Math.random() *
-						(playerMove.damageMax - playerMove.damageMin + 1)
-				) + playerMove.damageMin;
-			const opponentMoveDamage =
-				Math.floor(
-					Math.random() *
-						(opponentMove.damageMax - opponentMove.damageMin + 1)
-				) + opponentMove.damageMin;
+  console.log("This is Arenas", arenas);
+  console.log("This is Selected Arena", selectedArena);
 
-			playerMoveDamage > opponentMoveDamage
-				? setOpponentHealth(
-						opponentHealth -
-							(playerMoveDamage - opponentMove.defense)
-				  )
-				: setPlayerHealth(
-						playerHealth - (opponentMoveDamage - playerMove.defense)
-				  );
-			console.log(playerHealth);
-			console.log(opponentHealth);
-		}
-	};
+  useEffect(() => {
+    fetchArenas();
+  }, []);
 
-	console.log('Health out of the loop');
-	console.log(playerHealth);
-	console.log(opponentHealth);
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
 
-	useEffect(() => {
-		if (characters.length > 0) {
-			const moveTypes = ['punch', 'kick', 'block', 'specialMove'];
-			const randomMove = Math.floor(Math.random() * 4);
-			setOpponentMove(characters[0].moves[moveTypes[randomMove]]);
-		}
-	}, [playerMove]);
+  const compareMoves = () => {
+    setRoundTracker(roundTracker + 1);
+    if (playerMove.name == "Block" || opponentMove.name == "Block") {
+    } else {
+      const playerMoveDamage =
+        Math.floor(
+          Math.random() * (playerMove.damageMax - playerMove.damageMin + 1)
+        ) + playerMove.damageMin;
+      const opponentMoveDamage =
+        Math.floor(
+          Math.random() * (opponentMove.damageMax - opponentMove.damageMin + 1)
+        ) + opponentMove.damageMin;
 
-	useEffect(() => {
-		if (playerHealth <= 0) {
-			setResult('Loss');
-		}
-	}, [playerHealth]);
+      playerMoveDamage > opponentMoveDamage
+        ? setOpponentHealth(
+            opponentHealth - (playerMoveDamage - opponentMove.defense)
+          )
+        : setPlayerHealth(
+            playerHealth - (opponentMoveDamage - playerMove.defense)
+          );
+      console.log(playerHealth);
+      console.log(opponentHealth);
+    }
+  };
 
-	useEffect(() => {
-		if (opponentHealth <= 0) {
-			setResult('Victory');
-		}
-	}, [opponentHealth]);
+  console.log("Health out of the loop");
+  console.log(playerHealth);
+  console.log(opponentHealth);
 
-	return (
-		<div>
-			{characters.length > 0 ? (
-				<RoundCounterResultContainer
-					roundTracker={roundTracker}
-					result={result}
-				/>
-			) : (
-				''
-			)}
-			{characters.length > 0 ? (
-				<CharacterSelect
-					characters={characters}
-					setSelectedCharacter={setSelectedCharacter}
-				/>
-			) : (
-				'Loading Characters'
-			)}
-			{characters.length > 0 ? (
-				<CharacterImage
-					selectedCharacter={selectedCharacter}
-					opponentCharacter={opponentCharacter}
-				/>
-			) : (
-				'Loading Image'
-			)}
-			{characters.length > 0 ? (
-				<PlayerMovesModal
-					selectedCharacter={selectedCharacter}
-					setPlayerMove={setPlayerMove}
-				/>
-			) : (
-				'Loading Moves'
-			)}
-			<p>
-				<b>Your move: </b>
-			</p>
-			<p>{playerMove.name}</p>
-			<p>
-				<b>Opponents move: </b>
-			</p>
-			<p>{opponentMove.name}</p>
-			<button onClick={compareMoves}>FIGHT</button>
-			<p>
-				<b>Your health: </b>
-			</p>
-			<p>{playerHealth}</p>
-			<p>
-				<b>Opponents health: </b>
-			</p>
-			<p>{opponentHealth}</p>
-		</div>
-	);
+  useEffect(() => {
+    if (characters.length > 0) {
+      const moveTypes = ["punch", "kick", "block", "specialMove"];
+      const randomMove = Math.floor(Math.random() * 4);
+      setOpponentMove(characters[0].moves[moveTypes[randomMove]]);
+    }
+  }, [playerMove]);
+
+  useEffect(() => {
+    if (playerHealth <= 0) {
+      setResult("Loss");
+    }
+  }, [playerHealth]);
+
+  useEffect(() => {
+    if (opponentHealth <= 0) {
+      setResult("Victory");
+    }
+  }, [opponentHealth]);
+
+  return (
+    <div id="background" className={selectedArena.file_name}>
+      {characters.length > 0 ? (
+        <RoundCounterResultContainer
+          roundTracker={roundTracker}
+          result={result}
+        />
+      ) : (
+        ""
+      )}
+      {characters.length > 0 ? (
+        <CharacterSelect
+          characters={characters}
+          setSelectedCharacter={setSelectedCharacter}
+        />
+      ) : (
+        "Loading Characters"
+      )}
+      {characters.length > 0 ? (
+        <CharacterImage
+          selectedCharacter={selectedCharacter}
+          opponentCharacter={opponentCharacter}
+        />
+      ) : (
+        "Loading Image"
+      )}
+      {arenas.length > 0 ? (
+        <ArenaSelect arenas={arenas} setSelectedArena={setSelectedArena} />
+      ) : (
+        "Loading Arena"
+      )}
+      {arenas.length > 0 ? (
+        <ArenaImage selectedArena={selectedArena} />
+      ) : (
+        "Loading Image"
+      )}
+      {characters.length > 0 ? (
+        <PlayerMovesModal
+          selectedCharacter={selectedCharacter}
+          setPlayerMove={setPlayerMove}
+        />
+      ) : (
+        "Loading Moves"
+      )}
+      <p>
+        <b>Your move: </b>
+      </p>
+      <p>{playerMove.name}</p>
+      <p>
+        <b>Opponents move: </b>
+      </p>
+      <p>{opponentMove.name}</p>
+      <button onClick={compareMoves}>FIGHT</button>
+      <p>
+        <b>Your health: </b>
+      </p>
+      <p>{playerHealth}</p>
+      <p>
+        <b>Opponents health: </b>
+      </p>
+      <p>{opponentHealth}</p>
+    </div>
+  );
 }
 
 export default App;
