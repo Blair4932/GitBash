da0527f6c77b201866a4594d1c9143dd1f94a708
Blair Scott <blairs4932@outlook.com>
2023-09-07 11:07:19 +0100

    Added character gifs

--- client/src/components/CharacterSelect.jsx
+++ client/src/components/CharacterSelect.jsx
@@ -1,24 +1,30 @@
 const CharacterSelect = ({ characters, setSelectedCharacter }) => {
-
-	return (
-		<div className="character--grid">
-			{characters.map((character) => (
-				<div key={character._id} className={`character--tile ${character.file_name}`}>
-					<button
-						onClick={() => {
-							setSelectedCharacter(character);
-						}}
-						className="tile--button"
-					>
-					</button>
-					<div className="tile--info">
-						<img src={`./images/character_sprites/${character.file_name}/${character.file_name + "_default"}.png`} className="tile--icon"/>
-						<h2>{character.name}</h2>
-					</div>
-				</div>
-			))}
-		</div>
-	);
+  return (
+    <div className="character--grid">
+      {characters.map((character) => (
+        <div
+          key={character._id}
+          className={`character--tile ${character.file_name}`}
+        >
+          <button
+            onClick={() => {
+              setSelectedCharacter(character);
+            }}
+            className="tile--button"
+          ></button>
+          <div className="tile--info">
+            <img
+              src={`./images/character_sprites/${character.file_name}/${
+                character.file_name + "_default"
+              }.gif`}
+              className="tile--icon"
+            />
+            <h2>{character.name}</h2>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default CharacterSelect;
