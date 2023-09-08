a3499a177f9cf6dc8f41c802c221e85adcda8724
Josh Booker <josh.booker2@gmail.com>
2023-09-08 13:17:16 +0100

    added styling to round tracker

--- client/src/components/RoundCounter.jsx
+++ client/src/components/RoundCounter.jsx
@@ -1,12 +1,17 @@
 const RoundCounter = ({ roundTracker, result }) => {
 	return (
-		<div
-			className={`${
-				result === 'win' || result === 'loss' ? '' : 'round-tracker'
-			}`}
-		>
-			Round: {roundTracker}
-		</div>
+		<>
+			<div
+				className={`${
+					result === 'win' || result === 'loss' ? '' : 'round-tracker'
+				}`}
+			>
+				<h1>Round</h1>
+				<div className="round-number">
+					<h1>{roundTracker}</h1>
+				</div>
+			</div>
+		</>
 	);
 };
 
