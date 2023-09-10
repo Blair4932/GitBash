7f6c8a40002b437b062a0bd868aa3334af04041e
Josh Booker <josh.booker2@gmail.com>
2023-09-10 11:49:26 +0100

    edited and finished title page

--- client/src/components/TitlePage.jsx
+++ client/src/components/TitlePage.jsx
@@ -8,11 +8,23 @@ const TitlePage = () => {
 				<div className="hero-text">
 					<h3 className="fade-in-first">Welcome To</h3>
 					<h1 className="fade-in-second">GITBASH</h1>
-					<Link to="/fight">
-						<button className="enter-button glow-on-hover metal-button fade-in-third">
-							Enter the Arena
-						</button>
-					</Link>
+					<div className="enter-buttons">
+						<Link to="/login">
+							<button className="enter-button glow-on-hover metal-button fade-in-third">
+								Login
+							</button>
+						</Link>
+						<Link to="/fight">
+							<button className="enter-button glow-on-hover metal-button fade-in-third">
+								Fight
+							</button>
+						</Link>
+						<Link to="/new">
+							<button className="enter-button glow-on-hover metal-button fade-in-third">
+								Create User
+							</button>
+						</Link>
+					</div>
 				</div>
 			</div>
 		</>
